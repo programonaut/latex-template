01de50941c655694124d31c0c4ceef77649bb87c
Maximilian Kürschner <dev@maximilian-kuerschner.de>
2021-05-10 18:54:56 +0200

    start-thesis.init

---
+++ start-thesis.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Are u sure you want to execute this experiment?"
+echo ""
+
+echo "Enter current filename"
+read currFilename
+
+echo "Enter new filename"
+read newFilename
+
+mv "$currFilename.tex" "$newFilename.tex"
+echo "currFilename.tex changed to $newFilename.tex"
+for file in $(find . -name '*.tex'); 
+do 
+    echo "$file changed"
+    sed -i 's/$currFilename.tex/$newFilename.tex/' $file
+done
